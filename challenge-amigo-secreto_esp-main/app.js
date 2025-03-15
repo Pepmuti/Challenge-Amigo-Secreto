a4a6f03fe28caa7444c54bea579d6f14e3fb2dca
Pepmuti <gabrieltoledocorzo@hotmail.com>
2025-03-14 23:00:41 -0600

    Version Final
    Termino de proyecto de sortear amigo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,48 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let inputNombre = document.getElementById("amigo"); // Captura el campo de entrada
+    let nombre = inputNombre.value.trim(); // Elimina espacios en blanco
+
+    if (nombre === "") {
+        //El nombre está vacío
+        alert("Por favor, inserte un nombre.");
+        return;
+    } else {
+        // El ususario ha ingresado un nombre
+        amigos.push(nombre);
+        inputNombre.value = ""; // Limpia el campo de entrada
+        actualizarLista();
+    }
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpia la lista para evitar duplicados
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+    
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    
+    asignarTextoElemento("#resultado", `El amigo secreto sorteado es: ${amigoSorteado}`);
+    
+    amigos = [];
+    actualizarLista();
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
